a0e7b423a3e0a38247d4242cab3efc30b2c37c9e
James Morrison <trackerchum@aol.com>
2026-02-11 23:54:12 +0000

    chore: added button component supporting submit and cancel

---
+++ src/frontend/main-ui/src/components/button/Button.tsx
@@ -0,0 +1,30 @@
+import LoadingDots from '../loadingText';
+import './Button.scss';
+
+export type ButtonType = "Submit" | "Cancel";
+
+interface Props {
+    readonly text: string;
+    readonly buttonType?: ButtonType;
+    readonly onSubmit: () => void | Promise<void>;
+    readonly disabled?: boolean;
+    readonly className?: string;
+    readonly loading?: boolean;
+}
+
+const Button = ({ text, buttonType = "Submit", onSubmit, disabled = false, className, loading }: Props) => {
+    return <button
+        onClick={disabled || loading ? undefined : (e) => {
+            e.preventDefault()
+            onSubmit()
+        }}
+        disabled={disabled || loading}
+        className={"Button " + buttonType}
+    >
+        <>
+            {loading ? <LoadingDots /> : text}
+        </>
+    </button>;
+}
+
+export default Button;
