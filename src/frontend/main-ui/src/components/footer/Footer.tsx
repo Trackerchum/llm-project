f4d2aa2e7b0b0bf6ead91d861c4fdcb5ddad6f54
James Morrison <trackerchum@aol.com>
2026-02-11 16:04:53 +0000

    chore: added Footer component

---
+++ src/frontend/main-ui/src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import './Footer.scss';
+
+interface Props {
+
+}
+
+const Footer = ({ }: Props) => {
+    return <footer className='Footer h-10 bg-slate-700'>
+        <div className="container">
+
+        </div>
+    </footer>
+}
+
+export default Footer;
