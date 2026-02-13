6b4a0c28c259a65425819c32d771daff4f2baacf
James Morrison <trackerchum@aol.com>
2026-02-13 20:37:16 +0000

    feat: added ollama LLM, updated POC endpoint for testing

--- src/backend/server/src/controllers/HomeController.ts
+++ src/backend/server/src/controllers/HomeController.ts
@@ -7,8 +7,36 @@ export class HomeController extends BaseController {
     }
 
     setupRoutes = (app: Express) => {
-        app.get(this.baseUrl, (req, res) => {
-            res.send({response: 'Running...'});
+        app.get(this.baseUrl, async (req, res) => {
+            try {
+                const r = await fetch("http://llm-ollama:11434/api/generate", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        model: "llama3.2:1b",
+                        prompt: "How much wood could a woodchuck chuck if a woodchuck could chuck wood?",
+                        stream: false
+                    }),
+                });
+            
+                if (!r.ok) {
+                    const text = await r.text();
+                    return res.status(502).json({ error: "ollama_error", status: r.status, body: text });
+                }
+            
+                const data = await r.json();
+                return res.json({ ok: true, response: data.response });
+            } catch (err: any) {
+                console.log("Error on api/generate: ", err);
+                return res.status(502).json({
+                    ok: false,
+                    error: String(err?.message ?? err),
+                    cause: {
+                        message: String(err?.cause?.message ?? ""),
+                        code: err?.cause?.code,
+                    },
+                });
+            }
         });
     }
 }
--- src/frontend/main-ui/src/pages/homePage/HomePage.tsx
+++ src/frontend/main-ui/src/pages/homePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from '../../components/button';
 import { Client } from '../../fetch';
 import './HomePage.scss';
@@ -6,11 +7,19 @@ const client = new Client("/api");
 
 const HomePage = () => {
 
+    const [isLoading, setLoading] = useState(false)
+
     return <div className="homePage">
         <h1>Home</h1>
-        <Button text='Submit Test' onSubmit={() => {
-            client.get("/").then(response => console.log(response));
-        }} />
+        <Button text='Submit Test'
+            loading={isLoading}
+            onSubmit={() => {
+                setLoading(true);
+                client.get("/").then(response => {
+                    setLoading(false);
+                    console.log(response);
+                });
+            }}/>
     </div>
 }
 
