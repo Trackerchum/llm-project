fc05bc6b273d3fa458810dabcc70c303076fe0f8
James Morrison <trackerchum@aol.com>
2026-03-01 16:01:36 +0000

    chore: added basic MCP page scaffold

---
+++ src/frontend/main-ui/src/pages/mcpPage/MCPPage.tsx
@@ -0,0 +1,11 @@
+import "./MCPPage.scss";
+
+const MCPPage = () => {
+    return (
+        <div className="mcpPage">
+            <h1>MCP</h1>
+        </div>
+    );
+};
+
+export default MCPPage;
---
+++ src/frontend/main-ui/src/pages/mcpPage/index.tsx
@@ -0,0 +1,3 @@
+import MCPPage from "./MCPPage";
+
+export default MCPPage;
