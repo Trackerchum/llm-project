8379ede0dd9ad546a9dc907f9086d87d4515cf6f
James Morrison <trackerchum@aol.com>
2026-03-06 16:33:21 +0000

    chore: added chat method for OllamaClient

--- src/backend/shared/src/clients/ollamaClient/OllamaClient.ts
+++ src/backend/shared/src/clients/ollamaClient/OllamaClient.ts
@@ -1,10 +1,16 @@
+import { Tool } from "../../types/mcp";
+import { Message } from "../../types/mcp/message";
+import { OllamaChatSuccess, OllamaError } from "../../types/ollama";
+
 export class OllamaClient {
 	private endpoint: string;
 	private model: string;
+	private stream: boolean;
 
 	constructor() {
 		this.endpoint = "http://llm-ollama:11434";
 		this.model = "llama3.2:1b";
+		this.stream = false;
 	}
 
 	generate = async (prompt: string) => {
@@ -15,7 +21,7 @@ export class OllamaClient {
 				body: JSON.stringify({
 					model: this.model,
 					prompt,
-					stream: false,
+					stream: this.stream,
 				}),
 			});
 
@@ -40,4 +46,44 @@ export class OllamaClient {
 			};
 		}
 	};
+
+	chat = async (chat: {
+		messages: Message[];
+		tools?: Tool[];
+	}): Promise<OllamaChatSuccess | OllamaError> => {
+		try {
+			console.log(JSON.stringify({
+				model: this.model,
+				stream: this.stream,
+				...chat
+			}));
+			const response = await fetch(new URL("/api/chat", this.endpoint).toString(), {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					model: this.model,
+					stream: this.stream,
+					...chat
+				}),
+			});
+			if (!response.ok) {
+				const text = await response.text();
+				return {
+					ok: false,
+					error: "ollama_error",
+					statusCode: response.status,
+					body: text,
+				};
+			}
+
+			return response.json();
+		} catch (err: any) {
+			return {
+				ok: false,
+				error: String(err?.message ?? err),
+				statusCode: err?.cause?.code ?? 500,
+				body: String(err?.cause?.message ?? ""),
+			};
+		}
+	}
 }
