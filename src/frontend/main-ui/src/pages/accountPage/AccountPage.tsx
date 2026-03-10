b9e74445f14b226e9c9aef79155b610d82af20fc
James Morrison <trackerchum@aol.com>
2026-03-10 21:39:33 +0000

    feat: added account page with logout button

--- src/frontend/main-ui/src/pages/accountPage/AccountPage.tsx
+++ src/frontend/main-ui/src/pages/accountPage/AccountPage.tsx
@@ -1,11 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../globalProvider';
+import HorizontalSpacer from '../../components/horizontalSpacer';
 import "./AccountPage.scss";
+import Button from '../../components/button';
 
 const AccountPage = () => {
+	const navigate = useNavigate();
+	const { getUser, logoutUser } = useAuth();
+	const user = getUser();
+	if (!user && typeof window !== "undefined") {
+		navigate('/login');
+	}
+
 	return (
-		<div className="accountPage">
-			<h1>Account page</h1>
+		<div className='accountPage'>
+			<h1>My Account</h1>
+			<HorizontalSpacer />
+			<div>
+				<p>First name: {user?.firstName}</p>
+				<p>Last name: {user?.lastName}</p>
+				<p>Email: {user?.email}</p>
+			</div>
+			<HorizontalSpacer />
+			<Button text='Logout'
+				buttonType={"Cancel"}
+				onSubmit={() => {
+					logoutUser();
+					navigate('/');
+				}}
+			/>
 		</div>
 	);
-};
+}
 
 export default AccountPage;
