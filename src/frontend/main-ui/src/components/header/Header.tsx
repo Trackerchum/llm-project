f9072adc2190f974e143c3568fae2d86915e40a6
James Morrison <trackerchum@aol.com>
2026-03-10 22:46:58 +0000

    chore: added breaks between navbar items

--- src/frontend/main-ui/src/components/header/Header.tsx
+++ src/frontend/main-ui/src/components/header/Header.tsx
@@ -4,13 +4,18 @@ import "./Header.scss";
 
 const Header = () => {
 	const { user } = useAuth();
+
 	return (
 		<header className="Header">
 			<div className="container">
 				<div className="navBar">
+					<p>|</p>
 					<a href="/">{appName}</a>
+					<p>|</p>
 					<a href="/chat">Chat</a>
-					<a href="/mcp">MCP</a>
+					<p>|</p>
+					<a href="/mcp">MCP test</a>
+					<p>|</p>
 				</div>
 				<div>{user ? <a href="/account">Account</a> : <a href="/login">Login/sign up</a>}</div>
 			</div>
