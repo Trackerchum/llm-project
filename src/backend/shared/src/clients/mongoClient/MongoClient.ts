9cc500954bb5d09e9d93e64ffb2d63a5f9278332
James Morrison <trackerchum@aol.com>
2026-03-11 15:20:56 +0000

    feat: added mongoClient

---
+++ src/backend/shared/src/clients/mongoClient/MongoClient.ts
@@ -0,0 +1,111 @@
+import {
+    Db,
+    Document,
+    Filter,
+    MongoClient as MongoNativeClient,
+    OptionalUnlessRequiredId,
+    UpdateFilter,
+    UpdateOptions,
+} from "mongodb";
+import { logger } from "../../logging";
+
+export class MongoClient {
+    private Client: MongoNativeClient;
+    private database: Db | null;
+    private connectionString: string;
+    private defaultDbName?: string;
+    private isConnected: boolean;
+
+    constructor(url: string, defaultDbName?: string) {
+        this.connectionString = url;
+        this.Client = new MongoNativeClient(url);
+        this.database = null;
+        this.defaultDbName = defaultDbName;
+        this.isConnected = false;
+    }
+
+    connect = async () => {
+        try {
+            await logger("MongoClient connect", () => this.Client.connect());
+            const db = this.getDatabase();
+            await logger("MongoClient ping", () => db.command({ ping: 1 }));
+        } catch (error) {
+            throw error;
+        }
+        this.isConnected = true;
+    };
+
+    destroy = async () => {
+        try {
+            await logger("MongoClient close", () => this.Client.close());
+        } catch (error) {
+            throw error;
+        }
+        this.database = null;
+        this.isConnected = false;
+    };
+
+    // Convenience method similar to Redis set/get for key-value style storage.
+    set = async <T>(collectionName: string, key: string, value: T) => {
+        const collection = this.getDatabase().collection<{ _id: string; value: T }>(collectionName);
+        return logger("MongoClient set", () =>
+            collection.updateOne({ _id: key }, { $set: { value } }, { upsert: true }),
+        );
+    };
+
+    get = async <T>(collectionName: string, key: string): Promise<T | null> => {
+        const collection = this.getDatabase().collection<{ _id: string; value: T }>(collectionName);
+        const document = await logger("MongoClient get", () => collection.findOne({ _id: key }));
+        return document?.value ?? null;
+    };
+
+    insertOne = <T extends Document>(collectionName: string, document: OptionalUnlessRequiredId<T>) => {
+        const collection = this.getDatabase().collection<T>(collectionName);
+        return logger("MongoClient insertOne", () => collection.insertOne(document));
+    };
+
+    findOne = <T extends Document>(collectionName: string, filter: Filter<T>) => {
+        const collection = this.getDatabase().collection<T>(collectionName);
+        return logger("MongoClient findOne", () => collection.findOne(filter));
+    };
+
+    updateOne = <T extends Document>(
+        collectionName: string,
+        filter: Filter<T>,
+        update: UpdateFilter<T>,
+        options?: UpdateOptions,
+    ) => {
+        const collection = this.getDatabase().collection<T>(collectionName);
+        return logger("MongoClient updateOne", () => collection.updateOne(filter, update, options));
+    };
+
+    deleteOne = <T extends Document>(collectionName: string, filter: Filter<T>) => {
+        const collection = this.getDatabase().collection<T>(collectionName);
+        return logger("MongoClient deleteOne", () => collection.deleteOne(filter));
+    };
+
+    private getDatabase = (): Db => {
+        if (this.database) {
+            return this.database;
+        }
+
+        const dbName = this.resolveDbName();
+        this.database = this.Client.db(dbName);
+        return this.database;
+    };
+
+    private resolveDbName = (): string => {
+        if (this.defaultDbName) {
+            return this.defaultDbName;
+        }
+
+        const path = new URL(this.connectionString).pathname.replace("/", "");
+        if (!path) {
+            throw new Error(
+                "Mongo database name is missing. Set it in MONGO_URL or pass defaultDbName to MongoClient.",
+            );
+        }
+
+        return path;
+    };
+}
---
+++ src/backend/shared/src/clients/mongoClient/index.ts
@@ -0,0 +1,3 @@
+import { MongoClient } from "./MongoClient";
+
+export { MongoClient }
