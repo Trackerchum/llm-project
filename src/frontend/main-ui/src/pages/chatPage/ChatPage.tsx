e23d498a3e1b20e4745df67ed6ea5dc6418fffd3
James Morrison <trackerchum@aol.com>
2026-03-12 22:51:27 +0000

    feat: added support for multiple chats on the frontend, pending new chat

--- src/frontend/main-ui/src/pages/chatPage/ChatPage.tsx
+++ src/frontend/main-ui/src/pages/chatPage/ChatPage.tsx
@@ -5,6 +5,8 @@ import "./ChatPage.scss";
 import TextInput from "../../components/form/textInput";
 import { useAuth } from "../../globalProvider";
 import LoadingText from "../../components/loadingText";
+import { ChatHistory, Message } from "../../types/chat";
+import Sidebar from "../../components/sidebar";
 
 const chatClient = new Client("/api/chat");
 
@@ -14,6 +16,8 @@ type ChatHistoriesResponse = {
 	userId: string;
 	chatHistories: Array<{
 		id: string;
+		// TODO assign on server
+		name: string,
 		messages: Array<{
 			role: "system" | "user" | "assistant" | "tool";
 			content: string;
@@ -29,12 +33,8 @@ const HomePage = () => {
 	const [isFetchingChatHistory, setIsFetchingChatHistory] = useState(false);
 	const [isSubmittingPrompt, setIsSubmittingPrompt] = useState(false);
 	const [promptText, setPromptText] = useState("");
-	const [chatHistory, setChatHistory] = useState<
-		Array<{
-			host: "user" | "assistant";
-			text: string;
-		}>
-	>([]);
+	const [chatHistories, setChatHistories] = useState<Array<ChatHistory>>([]);
+	const [activeChatId, setActiveChatId] = useState("");
 
 	useEffect(() => {
 		if (user?.id) {
@@ -47,21 +47,39 @@ const HomePage = () => {
 					return;
 				}
 				// TODO support mutliple chat histories
-				if (response.data.chatHistories?.[0]) {
-					const histories = response.data.chatHistories[0].messages
-						.filter((message) => message.role === "assistant" || message.role === "user")
-						.map((message) => ({
-							host: message.role as "user" | "assistant",
-							text: message.content,
-						}));
+				if (response.data.chatHistories?.length > 0) {
+					setActiveChatId(response.data.chatHistories[0].id);
+					const histories = response.data.chatHistories.map(chatHistory => ({
+						id: chatHistory.id,
+						name: chatHistory.name ?? chatHistory.id,
+						messages: chatHistory.messages
+							.filter((message) => message.role === "assistant" || message.role === "user")
+							.map((message) => ({
+								host: message.role as "user" | "assistant",
+								text: message.content,
+							}))
+					}))
 					if (histories) {
-						setChatHistory(histories);
+						setChatHistories(histories);
 					}
 				}
 				setIsFetchingChatHistory(false);
 			});
 		}
 	}, [user]);
+
+	const updateChatHistoryImmutable = (previousState: ChatHistory[], chatId: string, newMessage: Message): ChatHistory[] => {
+		return previousState.map((chatHistory) => {
+			if (chatHistory.id !== chatId) {
+				return chatHistory;
+			}
+
+			return {
+				...chatHistory,
+				messages: [...chatHistory.messages, newMessage],
+			};
+		});
+	}
 
 	const submitPrompt = () => {
 		if (!promptText) {
@@ -72,56 +90,66 @@ const HomePage = () => {
 			window.alert("You must be signed in");
 			return;
 		}
-		setChatHistory((prev) => [...prev, { host: "user", text: promptText }]);
+		setChatHistories((prev) => updateChatHistoryImmutable(prev, activeChatId, { host: "user", text: promptText }));
 		setIsSubmittingPrompt(true);
 		setPromptText("");
 
 		chatClient.post<{ response: string }>("", { prompt: promptText, userId: user.id }).then((response) => {
 			setIsSubmittingPrompt(false);
 			if (response.isError) {
-				setChatHistory((prev) => [...prev, { host: "assistant", text: `Error: ${response.error}` }]);
+				// TODO handle error
 				return;
 			}
-			setChatHistory((prev) => [...prev, { host: "assistant", text: response.data.response }]);
+			setChatHistories((prev) => updateChatHistoryImmutable(prev, activeChatId, { host: "assistant", text: response.data.response }));
 		});
 	};
 
 	return (
-		<div className="chatPage">
-			{isFetchingChatHistory ? (
+		<Sidebar
+			children={isFetchingChatHistory ? (
 				<LoadingText text="Fetching chat history" />
-			) : (
-				<>
-					<h1>Chat</h1>
-					<div className="chatWindow">
-						{chatHistory.length !== 0 && (
-							<div className="chat">
-								{chatHistory.map((message, n) => (
-									<p key={n} className={message.host}>
-										{message.text}
-									</p>
-								))}
-							</div>
-						)}
-						<TextInput
-							labelText="Prompt: "
-							name="promptText"
-							value={promptText}
-							onChange={(newValue: string) => {
-								setPromptText(newValue);
-							}}
-							onKeyDown={(e: React.KeyboardEvent) => {
-								if (e.key === "Enter") {
-									e.preventDefault();
-									submitPrompt();
-								}
-							}}
-						/>
-					</div>
-					<Button text="Submit Prompt" loading={isSubmittingPrompt} onSubmit={submitPrompt} />
-				</>
+			) : (<ul className="chatList">{chatHistories.map(chatHistory => (
+				<li key={chatHistory.id} className={chatHistory.id === activeChatId ? "active" : ""}>
+					{chatHistory.name ?? chatHistory.id ?? "New Chat"}
+				</li>)
 			)}
-		</div>
+				{/* <li onClick={() => {}}>+ New Chat</li> */}
+			</ul>)}
+			page={<div className="chatPage">
+				{isFetchingChatHistory ? (
+					<LoadingText text="Fetching chat history" />
+				) : (
+					<>
+						<h1>Chat</h1>
+						<div className="chatWindow">
+							{chatHistories.length !== 0 && (
+								<div className="chat">
+									{chatHistories[0].messages.map((message, n) => (
+										<p key={n} className={message.host}>
+											{message.text}
+										</p>
+									))}
+								</div>
+							)}
+							<TextInput
+								labelText="Prompt: "
+								name="promptText"
+								value={promptText}
+								onChange={(newValue: string) => {
+									setPromptText(newValue);
+								}}
+								onKeyDown={(e: React.KeyboardEvent) => {
+									if (e.key === "Enter") {
+										e.preventDefault();
+										submitPrompt();
+									}
+								}}
+							/>
+						</div>
+						<Button text="Submit Prompt" loading={isSubmittingPrompt} onSubmit={submitPrompt} />
+					</>
+				)}
+			</div>} />
 	);
 };
 
