5098f738f7a4f689363ced1ae67ad8756b483e60
James Morrison <trackerchum@aol.com>
2026-03-13 14:31:16 +0000

    chore: added generateHash function

---
+++ src/frontend/main-ui/src/helpers/generateHash.ts
@@ -0,0 +1,10 @@
+const generateHash = (input: string) => {
+    let hash = 0;
+    for (const char of input) {
+        hash = (hash << 5) - hash + char.charCodeAt(0);
+        hash |= 0; // Constrain to 32bit integer
+    }
+    return hash;
+};
+
+export { generateHash }
