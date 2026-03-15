cfb3ad0be5bb471f3e4c0beb2c9e42c2fdd0286b
James Morrison <trackerchum@aol.com>
2026-03-15 15:44:44 +0000

    chore: added notifications and methods to global state

--- src/frontend/main-ui/src/globalProvider/GlobalProvider.tsx
+++ src/frontend/main-ui/src/globalProvider/GlobalProvider.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { User } from "../models/user";
 import { userCookieKey } from "../helpers/constants";
+import { Notification } from "../types/notification";
 
 interface AuthState {
 	user: User | null;
@@ -9,14 +10,22 @@ interface AuthState {
 	logoutUser: () => Promise<void>;
 }
 
+interface NotificationsState {
+	notifications: Notification[],
+	addNotification: (notification: Notification) => void,
+	removeNotification: (notificationId: string) => void
+}
+
 interface GlobalState {
 	authState: AuthState;
+	notificationsState: NotificationsState;
 }
 
 const globalState = createContext<GlobalState | null>(null);
 
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
 	const [user, setUserState] = useState<AuthState["user"]>(null);
+	const [notifications, setNotifications] = useState<NotificationsState["notifications"]>([]);
 
 	useEffect(() => {
 		window.cookieStore.get(userCookieKey).then((cookie: CookieListItem | null) => {
@@ -45,6 +54,17 @@ export function GlobalProvider({ children }: { children: React.ReactNode }) {
 					getUser: () => user,
 					logoutUser,
 				},
+				notificationsState: {
+					notifications,
+					addNotification: (notification: Notification) => setNotifications(prev => [...prev, notification]),
+					removeNotification: (notificationId: string) => setNotifications(prev => {
+						const notificationIndex = prev.findIndex(notification => notificationId === notification.id);
+						if (notificationIndex !== -1) {
+							return [...prev.slice(0, notificationIndex), ...prev.slice(notificationIndex + 1)];
+						}
+						return notifications;
+					})
+				}
 			}}
 		>
 			{children}
@@ -56,4 +76,10 @@ export function useAuth(): AuthState {
 	const ctx = useContext(globalState);
 	if (!ctx) throw new Error("useAuth must be used inside AuthProvider");
 	return ctx.authState;
+}
+
+export function useNotifications(): NotificationsState {
+	const ctx = useContext(globalState);
+	if (!ctx) throw new Error("useNotifications must be used inside AuthProvider");
+	return ctx.notificationsState;
 }
