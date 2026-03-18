5db676283ec1f8a80b3da635312066ee227dd533
James Morrison <trackerchum@aol.com>
2026-03-18 22:34:12 +0000

    chore: split logger time_elapsed into s and ms, rounded to nearest ms

--- src/backend/shared/src/logging/logger.ts
+++ src/backend/shared/src/logging/logger.ts
@@ -1,5 +1,13 @@
 import { performance } from "node:perf_hooks";
 
+const formatElapsedTime = (elapsedMs: number): string => {
+	if (elapsedMs > 1000) {
+		return `${(elapsedMs / 1000).toFixed(3)} seconds`;
+	}
+
+	return `${Math.floor(elapsedMs)} milliseconds`;
+};
+
 export const logger = async <T>(
 	actionName: string,
 	fn: (...args: any[]) => Promise<T>,
@@ -12,7 +20,7 @@ export const logger = async <T>(
 			JSON.stringify({
 				action_name: actionName,
 				result: "SUCCESS",
-				time_elapsed: `${performance.now() - start}ms`,
+				time_elapsed: formatElapsedTime(performance.now() - start),
 				...extraInfo,
 			}),
 		);
@@ -23,7 +31,7 @@ export const logger = async <T>(
 				action_name: actionName,
 				result: "ERROR",
 				error,
-				time_elapsed: `${performance.now() - start}ms`,
+				time_elapsed: formatElapsedTime(performance.now() - start),
 				...extraInfo,
 			}),
 		);
